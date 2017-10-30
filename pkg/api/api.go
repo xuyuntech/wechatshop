b0acc583c94103b5d45d2d88d4da6ce71b1f0c82
cabernety <330785652@qq.com>
2017-10-30 09:34:48 +0800

    add api

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,1 +1,19 @@
 package api
+
+type api struct {
+	listen string
+}
+
+type ApiConfig struct {
+	Listen string
+}
+
+func NewApi(config *ApiConfig) (*api, error) {
+	return &api{
+		listen: config.Listen,
+	}, nil
+}
+
+func (*api) Run() error {
+	return nil
+}
